0a60d86afcb948157a3f7a579c0f56912ca0fd18
Mona <mona.uppi@gmx.de>
2024-12-13 20:31:41 +0100

    🧚🏻‍♀️ Add nintendo-mid component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NintendoTopComponent } from './nintendo-top/nintendo-top.component';
+import { NintendoMidComponent } from './nintendo-mid/nintendo-mid.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [NintendoTopComponent],
+  imports: [NintendoTopComponent, NintendoMidComponent],
 })
 export class AppComponent {
   messageHeader: string = 'Mona';
---
+++ src/app/nintendo-mid/nintendo-mid.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'nintendo-mid',
+  templateUrl: 'nintendo-mid.component.html',
+})
+export class NintendoMidComponent {}
