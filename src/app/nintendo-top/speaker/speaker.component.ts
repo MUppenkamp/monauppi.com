94229dab1d272e062d170a465551e43b6c939ad1
m.uppenkamp <35469083+MUppenkamp@users.noreply.github.com>
2025-03-13 13:30:17 +0100

    🔧 Rename files

---
+++ src/app/nintendo-top/speaker/speaker.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'speaker',
+  templateUrl: 'speaker.component.html',
+})
+export class SpeakerComponent {}
--- src/app/nintendo-top/ventilation/ventilation.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'ventilation',
-  templateUrl: 'ventilation.component.html',
-})
-export class VentilationComponent {}
