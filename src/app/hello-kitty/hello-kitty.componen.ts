86870b14ba4ea535d319afa6b92ed96afb730719
Mona <35469083+MUppenkamp@users.noreply.github.com>
2025-03-16 17:07:27 +0100

    Add hello kitty

---
+++ src/app/hello-kitty/hello-kitty.componen.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hello-kitty',
+  templateUrl: 'hello-kitty.component.html',
+  styleUrl: 'hello-kitty.component.css',
+})
+export class HelloKittyComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
