166033e0f3c2a7c8d4de976a9a56e7674e32c6d4
Mona <35469083+MUppenkamp@users.noreply.github.com>
2025-03-16 17:07:42 +0100

    Add coming soon fallback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NintendoTopComponent } from './nintendo-top/nintendo-top.component';
 import { NintendoMidComponent } from './nintendo-mid/nintendo-mid.component';
 import { NintendoBotComponent } from "./nintendo-bot/nintendo-bot.component";
+import { HelloKittyComponent } from "./hello-kitty/hello-kitty.componen";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [NintendoTopComponent, NintendoMidComponent, NintendoBotComponent],
+  imports: [NintendoTopComponent, NintendoMidComponent, NintendoBotComponent, HelloKittyComponent],
 })
 export class AppComponent {
   messageHeader: string = 'Mona';
